4533fdbe23c84c167d9a6cd232c520e5fbf7c4ce
zeroarticle <150916818+zeroarticle@users.noreply.github.com>
2026-08-03 18:18:09 +0500

    task25a

---
+++ solutions/task25.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=0, b=0;
+    cin>>a;
+    cin>>b;
+    if(a>b){
+    	cout<<">";
+    }
+    if(a<b){
+        cout<<"<";
+    }
+    if(a==b){
+        cout<<"=";
+    }
+}
