efe9383bd41c468ac4eb4fb1bda4dfe4d768b116
zeroarticle <150916818+zeroarticle@users.noreply.github.com>
2026-08-03 18:04:37 +0500

    task10a

---
+++ solutions/task10.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long a,b,c,d; cin>>a>>b>>c>>d;
+    for(long long x=-100;x<=100;x++){
+        if(a*x*x*x+b*x*x+c*x+d==0){
+            cout<<x<<' ';
+        }
+    }
+}
